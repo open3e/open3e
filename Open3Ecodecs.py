5b401773b03868315920c5facf2838e7089aca41
Phil <122479122+philippoo66@users.noreply.github.com>
2023-11-01 12:19:58 +0100

    O3EUtf8 Hotfix srcolled back

--- Open3Ecodecs.py
+++ Open3Ecodecs.py
@@ -144,9 +144,15 @@ class O3EUtf8(udsoncan.DidCodec):
     def decode(self, string_bin: bytes) -> Any:
         if(flag_rawmode == True): 
             return RawCodec.decode(self, string_bin)
-        mystr = string_bin[self.offset:self.offset+self.string_len].decode('utf-8')
-        return mystr.replace('\x00', '')
-
+        #mystr = string_bin[self.offset:self.offset+self.string_len].decode('utf-8')
+        #return mystr.replace('\x00', '')
+        mystr=""
+        for i in range(self.offset, self.string_len):
+            if(string_bin[i] == 0):
+                break
+            mystr += string_bin[i:i+1].decode('utf-8')
+        return mystr
+       
     def __len__(self) -> int:
         return self.string_len
 
