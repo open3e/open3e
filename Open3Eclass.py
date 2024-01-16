45667e03581b6df7c39cbdb2f50d4db28ac5aac0
MyHomeMyData <juergen.bonfert@gmail,com>
2024-01-16 12:39:03 +0100

    Fix for high load issue

--- Open3Eclass.py
+++ Open3Eclass.py
@@ -87,7 +87,7 @@ class O3Eclass():
         else:
             # Refer to isotp documentation for full details about parameters
             isotp_params = {
-                'stmin': 10,                            # Will request the sender to wait 32ms between consecutive frame. 0-127ms or 100-900ns with values from 0xF1-0xF9
+                'stmin': 10,                            # Will request the sender to wait 10ms between consecutive frame. 0-127ms or 100-900ns with values from 0xF1-0xF9
                 'blocksize': 0,                         # Request the sender to send 8 consecutives frames before sending a new flow control message
                 'wftmax': 0,                            # Number of wait frame allowed before triggering an error
                 'tx_data_length': 8,                    # Link layer (CAN layer) works with 8 byte payload (CAN 2.0)
@@ -107,7 +107,7 @@ class O3Eclass():
             bus = SocketcanBus(channel=can, bitrate=250000)                                          # Link Layer (CAN protocol)
             tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=ecutx, rxid=ecurx) # Network layer addressing scheme
             stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params)               # Network/Transport layer (IsoTP protocol)
-            stack.set_sleep_timing(0, 0)                                                        # Speed First (do not sleep)
+            stack.set_sleep_timing(0.01, 0.01)                                                  # Balancing speed and load
             conn = PythonIsoTpConnection(stack)                                                 # interface between Application and Transport layer
 
         # UDS setup ~~~~~~~~~~~~~~~~~~
