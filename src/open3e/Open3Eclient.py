33e9aa5c434fa6ffd6b3ededed21220b2909bb67
phphilippoo66 <philip.oebel@web.de>
2024-11-30 21:12:34 +0100

    r/w subs w listen (working basically, debug version)

--- src/open3e/Open3Eclient.py
+++ src/open3e/Open3Eclient.py
@@ -121,16 +121,21 @@ def main():
         return lst
 
     # gets did and sub from fraction tailing 'ecu.'
-    def get_didsub(s) -> tuple: # [did,sub=none]
+    def _get_didsub(v) -> tuple: # [did,sub=none]
         """
-        :param s: string representing did or did.sub
+        :param v: something representing did or did.sub
         :return: tuple of did,sub where sub=None if not given 
         """
-        parts = str(s).replace(' ','').split('.')
+        parts = str(v).replace(' ','').split('.')
         if (len(parts) > 1):
             return [parts[0],parts[1]]
-        return [s,None]
+        return [v,None]
           
+    def get_didsub(v) -> tuple:
+        r1,r2=_get_didsub(v)
+        print(type(v),r1,r2)
+        return r1,r2
+    
 
     def ensure_ecu(addr:int):
         if(not (addr in dicEcus)):
@@ -189,7 +194,6 @@ def main():
                         ensure_ecu(addr) 
                         for did in dids:
                             didsub = get_didsub(did)
-                            print(type(did),did,didsub)
                             #readbydid(addr, getint(did), json=(cd['mode']=='read-json'), raw=(cd['mode']=='read-raw'))
                             readbydid(addr, didsub[0], json=(cd['mode']=='read-json'), raw=(cd['mode']=='read-raw'), sub=didsub[1])
                             time.sleep(0.01)            # 10 ms delay before next request
