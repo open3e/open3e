0fc9e3fd413b0e25ab6740683495e6fc68bc8010
phphilippoo66 <philip.oebel@web.de>
2024-12-08 14:22:09 +0100

    ensured int with to_bytes

--- src/open3e/Open3Ecodecs.py
+++ src/open3e/Open3Ecodecs.py
@@ -67,7 +67,7 @@ class O3EInt(udsoncan.DidCodec):
             if (self.offset != 0):
                 raise("O3EInt.encode(): offset!=0 not implemented yet") 
             val = round(eval(str(string_ascii))*self.scale)    # convert submitted data to numeric value and apply scaling factor
-            string_bin = val.to_bytes(length=self.byte_width,byteorder="little",signed=self.signed)
+            string_bin = int(val).to_bytes(length=self.byte_width,byteorder="little",signed=self.signed)
             return string_bin
 
     def decode(self, string_bin: bytes) -> Any:
@@ -103,7 +103,7 @@ class O3EByteVal(udsoncan.DidCodec):
     def encode(self, string_ascii: Any) -> bytes:        
         if(flag_rawmode == True): 
             return RawCodec.encode(self, string_ascii)
-        string_bin = string_ascii.to_bytes(length=self.string_len,byteorder="little",signed=False)
+        string_bin = int(string_ascii).to_bytes(length=self.string_len,byteorder="little",signed=False)
         return string_bin
 
     def decode(self, string_bin: bytes) -> Any:
