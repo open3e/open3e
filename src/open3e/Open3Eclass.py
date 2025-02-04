bc2b25bf64f9d9029e5e3bef62e83078fc1d5f2e
phphilippoo66 <philip.oebel@web.de>
2025-02-04 12:51:46 +0100

    try 10

--- src/open3e/Open3Eclass.py
+++ src/open3e/Open3Eclass.py
@@ -1,17 +1,17 @@
 
 import udsoncan
-# from doipclient import DoIPClient
-# from doipclient.connectors import DoIPClientUDSConnector
-#from udsoncan.client import Client
+from doipclient import DoIPClient
+from doipclient.connectors import DoIPClientUDSConnector
+from udsoncan.client import Client
 from open3e.Open3EudsClient import Open3EudsClient
 from udsoncan.exceptions import *
 from udsoncan.services import *
 
-# from can.interface import Bus
+from can.interface import Bus
 from udsoncan.connections import PythonIsoTpConnection
-# from can.interfaces.socketcan import SocketcanBus
-# from can.interfaces.slcan import slcanBus
-# import isotp
+from can.interfaces.socketcan import SocketcanBus
+from can.interfaces.slcan import slcanBus
+import isotp
 
 from typing import Optional, Any
 import logging
@@ -39,26 +39,21 @@ def import_path(path):
 
 
 class O3Eclass():
-    def __init__(self,
-                 conn, 
-#                 bus,
-                 ecutx:int=0x680, ecurx:int=0,
-#                 doip:str=None, # doip mode if not empty  
-#                 can:str='can0',
-#                 slcan:str=None,
+    def __init__(self, ecutx:int=0x680, ecurx:int=0,
+                 doip:str=None, # doip mode if not empty  
+                 can:str='can0', 
                  dev=None
                 ):
 
-        self.conn = conn
-#        self.bus = bus
         self.tx = ecutx 
         self.dev = dev  # not necessary
         self.numdps = 0
-#        self.slcan = slcan
+        self.SLCANBUS = None
 
-#        # ECU addresses ~~~~~~~~~~~~~~~~~~
-#        if(ecurx == 0):
-#            ecurx = ecutx + 0x10
+        # ECU addresses ~~~~~~~~~~~~~~~~~~
+        if(ecurx == 0):
+            ecurx = ecutx + 0x10
+
 
         # load general datapoints table from open3e.Open3Edatapoints.py
         self.dataIdentifiers = dict(open3e.Open3Edatapoints.dataIdentifiers["dids"])            
@@ -104,66 +99,51 @@ class O3Eclass():
                 # for info
                 self.numdps = len(self.dataIdentifiers)
 
-        # # select CAN / DoIP ~~~~~~~~~~~~~~~~~~
-        # if(doip != None): #DoIP
-        #     self.conn = DoIPClientUDSConnector(DoIPClient(doip, ecutx))
-        # elif(slcan != None): #SLCAN Serial CAN Interface either locally via USB or remote via Telnet
-        #     isotp_params = {
-        #         'stmin': 10,                            # Will request the sender to wait 10ms between consecutive frame. 0-127ms or 100-900ns with values from 0xF1-0xF9
-        #         'blocksize': 0,                         # Request the sender to send 8 consecutives frames before sending a new flow control message
-        #         'wftmax': 0,                            # Number of wait frame allowed before triggering an error
-        #         'tx_data_length': 8,                    # Link layer (CAN layer) works with 8 byte payload (CAN 2.0)
-        #         'tx_data_min_length': 8,                # Minimum length of CAN messages. When different from None, messages are padded to meet this length. Works with CAN 2.0 and CAN FD.
-        #         'tx_padding': 0,                        # Will pad all transmitted CAN messages with byte 0x00.
-        #         'rx_flowcontrol_timeout': 1000,         # Triggers a timeout if a flow control is awaited for more than 1000 milliseconds
-        #         'rx_consecutive_frame_timeout': 1000,   # Triggers a timeout if a consecutive frame is awaited for more than 1000 milliseconds
-        #         'override_receiver_stmin': None,        # When sending, respect the stmin requirement of the receiver if set to None.
-        #         'max_frame_size': 4095,                 # Limit the size of receive frame.
-        #         'can_fd': False,                        # Does not set the can_fd flag on the output CAN messages
-        #         'bitrate_switch': False,                # Does not set the bitrate_switch flag on the output CAN messages
-        #         'rate_limit_enable': False,             # Disable the rate limiter
-        #         'rate_limit_max_bitrate': 1000000,      # Ignored when rate_limit_enable=False. Sets the max bitrate when rate_limit_enable=True
-        #         'rate_limit_window_size': 0.2,          # Ignored when rate_limit_enable=False. Sets the averaging window size for bitrate calculation when rate_limit_enable=True
-        #         'listen_mode': False                    # Does not use the listen_mode which prevent transmission.
-        #     }
-        #     # Reuse global slcanbus instance for all instances of O3Eclass because COM port can not be bound multiple times
-        #     if O3Eclass.SLCANBUS == None:
-        #         print("Creating new SLCANBUS Instance for ECU: " + str(ecutx))
-        #         bus = slcanBus(channel=slcan, tty_baudrate=115200, bitrate=250000)
-        #         O3Eclass.SLCANBUS = bus
-        #     else:
-        #         print("Reusing SLCANBUS Instance for ECU: " + str(ecutx))
-        #         bus =  O3Eclass.SLCANBUS
+        # Bus setup - select CAN / DoIP ~~~~~~~~~~~~~~~~~~
+        if(doip != None): #DoIP
+            self.conn = DoIPClientUDSConnector(DoIPClient(doip, ecutx))
+        elif(slcan != None): #SLCAN Serial CAN Interface either locally via USB or remote via Telnet
+            # Reuse global slcanbus instance for all instances of O3Eclass because COM port can not be bound multiple times
+            if self.SLCANBUS == None:
+                print("Creating new SLCANBUS Instance for ECU: " + str(ecutx))
+                bus = slcanBus(channel=slcan, tty_baudrate=115200, bitrate=250000)
+                self.SLCANBUS = bus
+            else:
+                print("Reusing SLCANBUS Instance for ECU: " + str(ecutx))
+                bus =  self.SLCANBUS
 
-        #     tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=ecutx, rxid=ecurx) # Network layer addressing scheme
-        #     stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params)               # Network/Transport layer (IsoTP protocol)
-        #     stack.set_sleep_timing(0.01, 0.01)                                                  # Balancing speed and load
-        #     self.conn = PythonIsoTpConnection(stack)   
-        # else:
-        #     # Refer to isotp documentation for full details about parameters
-        #     isotp_params = {
-        #         'stmin': 10,                            # Will request the sender to wait 10ms between consecutive frame. 0-127ms or 100-900ns with values from 0xF1-0xF9
-        #         'blocksize': 0,                         # Request the sender to send 8 consecutives frames before sending a new flow control message
-        #         'wftmax': 0,                            # Number of wait frame allowed before triggering an error
-        #         'tx_data_length': 8,                    # Link layer (CAN layer) works with 8 byte payload (CAN 2.0)
-        #         'tx_data_min_length': 8,                # Minimum length of CAN messages. When different from None, messages are padded to meet this length. Works with CAN 2.0 and CAN FD.
-        #         'tx_padding': 0,                        # Will pad all transmitted CAN messages with byte 0x00.
-        #         'rx_flowcontrol_timeout': 1000,         # Triggers a timeout if a flow control is awaited for more than 1000 milliseconds
-        #         'rx_consecutive_frame_timeout': 1000,   # Triggers a timeout if a consecutive frame is awaited for more than 1000 milliseconds
-        #         'override_receiver_stmin': None,        # When sending, respect the stmin requirement of the receiver if set to None.
-        #         'max_frame_size': 4095,                 # Limit the size of receive frame.
-        #         'can_fd': False,                        # Does not set the can_fd flag on the output CAN messages
-        #         'bitrate_switch': False,                # Does not set the bitrate_switch flag on the output CAN messages
-        #         'rate_limit_enable': False,             # Disable the rate limiter
-        #         'rate_limit_max_bitrate': 1000000,      # Ignored when rate_limit_enable=False. Sets the max bitrate when rate_limit_enable=True
-        #         'rate_limit_window_size': 0.2,          # Ignored when rate_limit_enable=False. Sets the averaging window size for bitrate calculation when rate_limit_enable=True
-        #         'listen_mode': False                    # Does not use the listen_mode which prevent transmission.
-        #     }
-        #     bus = SocketcanBus(channel=can, bitrate=250000)                                     # Link Layer (CAN protocol)
-        #     tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=ecutx, rxid=ecurx) # Network layer addressing scheme
-        #     stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params)               # Network/Transport layer (IsoTP protocol)
-        #     stack.set_sleep_timing(0.01, 0.01)                                                  # Balancing speed and load
-        #     self.conn = PythonIsoTpConnection(stack)                                            # interface between Application and Transport layer
+            tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=ecutx, rxid=ecurx) # Network layer addressing scheme
+            stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params())               # Network/Transport layer (IsoTP protocol)
+            stack.set_sleep_timing(0.01, 0.01)                                                  # Balancing speed and load
+            self.conn = PythonIsoTpConnection(stack)   
+        else:
+            bus = SocketcanBus(channel=can, bitrate=250000)                                     # Link Layer (CAN protocol)
+            tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=ecutx, rxid=ecurx) # Network layer addressing scheme
+            stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params())               # Network/Transport layer (IsoTP protocol)
+            stack.set_sleep_timing(0.01, 0.01)                                                  # Balancing speed and load
+            self.conn = PythonIsoTpConnection(stack)                                            # interface between Application and Transport layer
+
+        def isotp_params():
+            # Refer to isotp documentation for full details about parameters
+            return {
+                'stmin': 10,                            # Will request the sender to wait 10ms between consecutive frame. 0-127ms or 100-900ns with values from 0xF1-0xF9
+                'blocksize': 0,                         # Request the sender to send 8 consecutives frames before sending a new flow control message
+                'wftmax': 0,                            # Number of wait frame allowed before triggering an error
+                'tx_data_length': 8,                    # Link layer (CAN layer) works with 8 byte payload (CAN 2.0)
+                'tx_data_min_length': 8,                # Minimum length of CAN messages. When different from None, messages are padded to meet this length. Works with CAN 2.0 and CAN FD.
+                'tx_padding': 0,                        # Will pad all transmitted CAN messages with byte 0x00.
+                'rx_flowcontrol_timeout': 1000,         # Triggers a timeout if a flow control is awaited for more than 1000 milliseconds
+                'rx_consecutive_frame_timeout': 1000,   # Triggers a timeout if a consecutive frame is awaited for more than 1000 milliseconds
+                'override_receiver_stmin': None,        # When sending, respect the stmin requirement of the receiver if set to None.
+                'max_frame_size': 4095,                 # Limit the size of receive frame.
+                'can_fd': False,                        # Does not set the can_fd flag on the output CAN messages
+                'bitrate_switch': False,                # Does not set the bitrate_switch flag on the output CAN messages
+                'rate_limit_enable': False,             # Disable the rate limiter
+                'rate_limit_max_bitrate': 1000000,      # Ignored when rate_limit_enable=False. Sets the max bitrate when rate_limit_enable=True
+                'rate_limit_window_size': 0.2,          # Ignored when rate_limit_enable=False. Sets the averaging window size for bitrate calculation when rate_limit_enable=True
+                'listen_mode': False                    # Does not use the listen_mode which prevent transmission.
+            }
+
 
         # UDS setup ~~~~~~~~~~~~~~~~~~
         udsoncan.setup_logging()
@@ -348,3 +328,5 @@ class O3Eclass():
 
     def close(self):
         self.uds_client.close()
+        if(self.SLCANBUS is not None):
+            self.SLCANBUS.shutdown()
