99c289126db37f6687ba2c9ab24ea41c09d751fb
Luca Graf <lucagraf@gmx.de>
2025-05-03 14:29:02 +0200

    add device config for integration tests.

---
+++ tests/integration/test_data/open3e_device_config/Open3Edatapoints_6a1.py
@@ -0,0 +1,17 @@
+import open3e.Open3Ecodecs
+from open3e.Open3Ecodecs import *
+
+dataIdentifiers = {
+    "name": "EMCUMASTER",
+    "dids" :
+    {
+        256 : None,
+        262 : None,
+        382 : None,
+        505 : None,
+        507 : None,
+        592 : None,
+        604 : None,
+        2214 : None,
+    }
+}
