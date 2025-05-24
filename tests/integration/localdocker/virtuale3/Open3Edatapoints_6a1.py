3b5e8e36860b432368e0ce2d9ad3a8b51ca918ce
Luca Graf <lucagraf@gmx.de>
2025-05-24 11:27:51 +0200

    add virtuale3 device config add mount in test environment.

---
+++ tests/integration/localdocker/virtuale3/Open3Edatapoints_6a1.py
@@ -0,0 +1,13 @@
+dataIdentifiers = {
+    "name": "EMCUMASTER",
+    "dids": {
+        256: None,
+        262: None,
+        382: None,
+        505: None,
+        507: None,
+        592: None,
+        604: None,
+        2214: None,
+    },
+}
